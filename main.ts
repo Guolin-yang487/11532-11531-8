1fe22441622e39799acb5ad37fdcf6cac649bf3a
Guolin-yang487 <65596845+Guolin-yang487@users.noreply.github.com>
2020-07-01 11:53:35 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+let line = 0
+function turnLeft () {
+    mbitbot.move_motor_pin(mbitbot.MPin.Mpin3, 30)
+    mbitbot.move_motor_pin(mbitbot.MPin.Mpin4, 0)
+    mbitbot.move_motor_pin(mbitbot.MPin.Mpin7, 30)
+    mbitbot.move_motor_pin(mbitbot.MPin.Mpin8, 0)
+}
+function forward () {
+    mbitbot.move_motor_pin(mbitbot.MPin.Mpin3, 30)
+    mbitbot.move_motor_pin(mbitbot.MPin.Mpin4, 0)
+    mbitbot.move_motor_pin(mbitbot.MPin.Mpin7, 30)
+    mbitbot.move_motor_pin(mbitbot.MPin.Mpin8, 0)
+}
+basic.forever(function () {
+    line = mbitbot.Me_Line_Follower_Sensor(mbitbot.Jpin.J3)
+    basic.showNumber(line)
+    if (line == 1) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
